a8f7c67b57b400a16ec6919cfd62d1cc1fe287f9
Zach Chan <zacharyleechan@gmail.com>
2020-10-06 21:35:24 -0700

    implemented _.reduce and passed all tests. Part 1 Complete

--- src/underbar.js
+++ src/underbar.js
@@ -128,6 +128,14 @@
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
+
+    let result = [];
+
+    _.each(collection, function(item) {
+      result.push(iterator(item));
+    });
+
+    return result;
   };
 
   /*
@@ -169,6 +177,23 @@
   //   }); // should be 5, regardless of the iterator function passed in
   //          No accumulator is given so the first element is used.
   _.reduce = function(collection, iterator, accumulator) {
+    var values = [];
+
+    _.each(collection, function (item) {
+      values.push(item);
+    });
+
+    if (accumulator === undefined) {
+      accumulator = values[0];
+      values.shift();
+    }
+
+    _.each(values, function(item){
+      accumulator = iterator(accumulator, item);
+    });
+
+    return accumulator;
+
   };
 
   // Determine if the array or object contains a given value (using `===`).
